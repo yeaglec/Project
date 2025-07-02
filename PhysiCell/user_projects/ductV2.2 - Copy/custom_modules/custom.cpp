dda05b07f5ac785c421cd45d5c5d63eab776733f
CameronY2001 <yeaglec@susqu.edu>
2025-07-02 18:19:34 -0400

    PhysiCell/Studio

---
+++ PhysiCell/user_projects/ductV2.2 - Copy/custom_modules/custom.cpp
@@ -0,0 +1,354 @@
+/*
+###############################################################################
+# If you use PhysiCell in your project, please cite PhysiCell and the version #
+# number, such as below:                                                      #
+#                                                                             #
+# We implemented and solved the model using PhysiCell (Version x.y.z) [1].    #
+#                                                                             #
+# [1] A Ghaffarizadeh, R Heiland, SH Friedman, SM Mumenthaler, and P Macklin, #
+#     PhysiCell: an Open Source Physics-Based Cell Simulator for Multicellu-  #
+#     lar Systems, PLoS Comput. Biol. 14(2): e1005991, 2018                   #
+#     DOI: 10.1371/journal.pcbi.1005991                                       #
+#                                                                             #
+# See VERSION.txt or call get_PhysiCell_version() to get the current version  #
+#     x.y.z. Call display_citations() to get detailed information on all cite-#
+#     able software used in your PhysiCell application.                       #
+#                                                                             #
+# Because PhysiCell extensively uses BioFVM, we suggest you also cite BioFVM  #
+#     as below:                                                               #
+#                                                                             #
+# We implemented and solved the model using PhysiCell (Version x.y.z) [1],    #
+# with BioFVM [2] to solve the transport equations.                           #
+#                                                                             #
+# [1] A Ghaffarizadeh, R Heiland, SH Friedman, SM Mumenthaler, and P Macklin, #
+#     PhysiCell: an Open Source Physics-Based Cell Simulator for Multicellu-  #
+#     lar Systems, PLoS Comput. Biol. 14(2): e1005991, 2018                   #
+#     DOI: 10.1371/journal.pcbi.1005991                                       #
+#                                                                             #
+# [2] A Ghaffarizadeh, SH Friedman, and P Macklin, BioFVM: an efficient para- #
+#     llelized diffusive transport solver for 3-D biological simulations,     #
+#     Bioinformatics 32(8): 1256-8, 2016. DOI: 10.1093/bioinformatics/btv730  #
+#                                                                             #
+###############################################################################
+#                                                                             #
+# BSD 3-Clause License (see https://opensource.org/licenses/BSD-3-Clause)     #
+#                                                                             #
+# Copyright (c) 2015-2021, Paul Macklin and the PhysiCell Project             #
+# All rights reserved.                                                        #
+#                                                                             #
+# Redistribution and use in source and binary forms, with or without          #
+# modification, are permitted provided that the following conditions are met: #
+#                                                                             #
+# 1. Redistributions of source code must retain the above copyright notice,   #
+# this list of conditions and the following disclaimer.                       #
+#                                                                             #
+# 2. Redistributions in binary form must reproduce the above copyright        #
+# notice, this list of conditions and the following disclaimer in the         #
+# documentation and/or other materials provided with the distribution.        #
+#                                                                             #
+# 3. Neither the name of the copyright holder nor the names of its            #
+# contributors may be used to endorse or promote products derived from this   #
+# software without specific prior written permission.                         #
+#                                                                             #
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" #
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE   #
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  #
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE   #
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR         #
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF        #
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS    #
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN     #
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)     #
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE  #
+# POSSIBILITY OF SUCH DAMAGE.                                                 #
+#                                                                             #
+###############################################################################
+*/
+
+#include "./custom.h"
+#include <cfloat> 
+
+
+// Helper: find closest BM cell and return both pointer & distance
+std::pair<PhysiCell::Cell*, double> BM_Helper(PhysiCell::Cell* pCell)
+{
+    double min_d = DBL_MAX;
+    PhysiCell::Cell* pBM  = nullptr;
+
+    // Iterate only over nearby cells (not sure if state.neighbors works here)
+    for (auto pOther : pCell->state.neighbors) {
+        if (pOther->type != 1) continue;
+
+		// Might need to be 2D
+        double d = norm(pCell->position - pOther->position);
+        if (d < min_d) {
+            min_d = d;
+            pBM   = pOther;
+        }
+    }
+    return {pBM, min_d};
+}
+
+
+double distance_to_membrane(Cell* pCell,
+                             Phenotype& phenotype,
+                             double dt)
+{
+    if (pCell->type == 1) return 0.0;
+    auto [pBM, d] = BM_Helper(pCell);
+    return (pBM ? d : DBL_MAX);
+}
+
+// 2) Interaction callback: spring force via neighbor list
+
+void basement_membrane_interaction(Cell* pCell,
+                                   Phenotype& phenotype,
+                                   double dt)
+{
+    if (pCell->type == 1) return;
+
+    auto [pBM, d] = BM_Helper(pCell);
+    if (!pBM) return;
+
+    double rest_length = parameters.doubles("membrane_interaction_length");
+    double k           = parameters.doubles("membrane_adhesion_strength");
+
+	// Might need to compute in 2D
+    std::vector<double> dir = pCell->position - pBM->position;
+    normalize(&dir);
+    double Fmag = k * (d - rest_length);
+
+    for (int i = 0; i < 3; i++) {
+        pCell->velocity[i] += Fmag * dir[i];
+    }
+}
+
+
+void custom_rule( Cell* pCell, Phenotype& phenotype, double dt )
+{	
+
+	double d = distance_to_membrane( pCell, phenotype, dt );
+
+	double &cyto_rate = phenotype.volume.cytoplasmic_biomass_change_rate;
+	double &nuclear_rate = phenotype.volume.nuclear_biomass_change_rate;
+	double &fluid_rate = phenotype.volume.fluid_change_rate;
+
+	double n = pCell->state.neighbors.size();
+	double N = pCell->state.spring_attachments.size();
+	double Sat = parameters.doubles("saturation_neighbors");
+
+	double factor = 1.0 - (N / Sat);
+	if (factor < 0.0) factor = 0.0; // saturation
+
+
+	if( d > 0.0 ){
+		// outside of membrane, so set everything to zero
+		// This should stop proliferation (test)
+
+		cyto_rate = 0.0;
+		nuclear_rate = 0.0;
+		fluid_rate = 0.0;
+	}
+
+	else{	
+		
+		cyto_rate = pCell->custom_data["default_cyto_rate"] * factor;
+		nuclear_rate = pCell->custom_data["default_nuclear_rate"] * factor;
+		fluid_rate = pCell->custom_data["default_fluid_rate"] * factor;
+
+	}
+}
+
+std::vector<std::vector<double>> generate_boundary_shape(double a, double b, double amp, int freq, double R, int num_points){
+
+	std::vector<std::vector<double>> pts;
+	pts.reserve(num_points);
+    
+	// Generating points for a deformed ellipse like shape
+    for (int i = 0; i < num_points; i++) {
+        
+        double theta = 2.0 * M_PI * i / (num_points - 1);
+        
+        double r_x = a * (1.0 + amp * cos(freq * theta));
+        double r_y = b * (1.0 + amp * sin(freq * theta));
+        double x = r_x * cos(theta);
+        double y = r_y * sin(theta);
+
+        pts.push_back({ x, y, 0.0 });
+    }
+    return pts;
+}
+
+
+void create_cell_types( void )
+{
+	// set the random seed 
+	if (parameters.ints.find_index("random_seed") != -1)
+	{
+		SeedRandom(parameters.ints("random_seed"));
+	}
+	
+	/* 
+	   Put any modifications to default cell definition here if you 
+	   want to have "inherited" by other cell types. 
+	   
+	   This is a good place to set default functions. 
+	*/ 
+	
+	initialize_default_cell_definition(); 
+	cell_defaults.phenotype.secretion.sync_to_microenvironment( &microenvironment ); 
+	
+	cell_defaults.functions.volume_update_function = standard_volume_update_function;
+	cell_defaults.functions.update_velocity = standard_update_cell_velocity;
+
+	cell_defaults.functions.update_migration_bias = NULL; 
+	cell_defaults.functions.update_phenotype = NULL; // update_cell_and_death_parameters_O2_based; 
+	cell_defaults.functions.custom_cell_rule = NULL; 
+	cell_defaults.functions.contact_function = NULL; 
+ 
+	
+	// std::cout << "Cell type defaults set. (Test)" << std::endl;
+	/*
+	   This parses the cell definitions in the XML config file. 
+	*/
+	
+	initialize_cell_definitions_from_pugixml(); 
+
+	/*
+	   This builds the map of cell definitions and summarizes the setup. 
+	*/
+		
+	cell_defaults.functions.add_cell_basement_membrane_interactions = NULL; 
+	cell_defaults.functions.calculate_distance_to_membrane = NULL; 
+
+	build_cell_definitions_maps(); 
+
+	/*
+	   This intializes cell signal and response dictionaries 
+	*/
+
+	setup_signal_behavior_dictionaries(); 	
+
+	/*
+       Cell rule definitions 
+	*/
+
+	setup_cell_rules(); 
+
+	/* 
+	   Put any modifications to individual cell definitions here. 
+	   
+	   This is a good place to set custom functions. 
+	*/ 
+	
+	cell_defaults.functions.update_phenotype = phenotype_function; 
+	cell_defaults.functions.custom_cell_rule = custom_rule; 
+	cell_defaults.functions.contact_function = contact_function; 
+	
+	/*
+	   This builds the map of cell definitions and summarizes the setup. 
+	*/
+		
+	display_cell_definitions( std::cout ); 
+	
+	return; 
+}
+
+void setup_microenvironment( void )
+{
+	// set domain parameters 
+	
+	// put any custom code to set non-homogeneous initial conditions or 
+	// extra Dirichlet nodes here. 
+	
+	// initialize BioFVM 
+	
+	initialize_microenvironment(); 	
+	
+	return; 
+}
+
+void setup_tissue( void )
+{
+	double Xmin = microenvironment.mesh.bounding_box[0]; 
+	double Ymin = microenvironment.mesh.bounding_box[1]; 
+	double Zmin = microenvironment.mesh.bounding_box[2]; 
+
+	double Xmax = microenvironment.mesh.bounding_box[3]; 
+	double Ymax = microenvironment.mesh.bounding_box[4]; 
+	double Zmax = microenvironment.mesh.bounding_box[5]; 
+	
+	if( default_microenvironment_options.simulate_2D == true )
+	{
+		Zmin = 0.0; 
+		Zmax = 0.0; 
+	}
+	
+	double Xrange = Xmax - Xmin; 
+	double Yrange = Ymax - Ymin; 
+	double Zrange = Zmax - Zmin; 
+	
+	// create some of each type of cell 
+	
+	Cell* pC;
+	
+	for( int k=0; k < cell_definitions_by_index.size() ; k++ )
+	{
+		Cell_Definition* pCD = cell_definitions_by_index[k]; 
+		std::cout << "Placing cells of type " << pCD->name << " ... " << std::endl; 
+		for( int n = 0 ; n < parameters.ints("number_of_cells") ; n++ )
+		{
+			std::vector<double> position = {0,0,0}; 
+			position[0] = Xmin + UniformRandom()*Xrange; 
+			position[1] = Ymin + UniformRandom()*Yrange; 
+			position[2] = Zmin + UniformRandom()*Zrange; 
+			
+			pC = create_cell( *pCD ); 
+			pC->assign_position( position );
+		}
+	}
+	std::cout << std::endl; 
+	
+	// load cells from your CSV file (if enabled)
+	load_cells_from_pugixml();
+	set_parameters_from_distributions();
+
+	// Create new shape for the basement membrane
+	double R = parameters.doubles("membrane_radius");
+	int num_points = parameters.ints("membrane_num_points");
+	double a = 350.0, b = 250.0;
+	int n_pts = 200;
+	double amp = 0.1;   // put in header later
+	int lobes = 4;
+
+	auto BM_pts = generate_boundary_shape(a, b, amp, lobes, R, num_points);
+	Cell_Definition* pBM_def = cell_definitions_by_index[1];
+
+	for (auto &pt : BM_pts){
+		Cell* pBM = create_cell( *pBM_def );
+		pBM->assign_position( pt );
+	}
+
+	for (auto pCell : *all_cells){
+
+		// Stash the volume growth rates in custom data
+		pCell->custom_data["default_cyto_rate"] = pCell->phenotype.volume.cytoplasmic_biomass_change_rate;
+		pCell->custom_data["default_nuclear_rate"] = pCell->phenotype.volume.nuclear_biomass_change_rate;
+		pCell->custom_data["default_fluid_rate"] = pCell->phenotype.volume.fluid_change_rate;
+
+	}
+	
+	return; 
+}
+
+std::vector<std::string> my_coloring_function( Cell* pCell )
+{ return paint_by_number_cell_coloring(pCell); }
+
+void phenotype_function( Cell* pCell, Phenotype& phenotype, double dt )
+{ return; }
+
+void custom_function( Cell* pCell, Phenotype& phenotype , double dt )
+{ return; } 
+
+void contact_function( Cell* pMe, Phenotype& phenoMe , Cell* pOther, Phenotype& phenoOther , double dt )
+{ return; } 
